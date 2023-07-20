689d7831cc2dd0457b9b73f8cedb84af721b3839
Bright Eshun <eshunbright136@gmail.com>
2023-07-20 15:26:46 +0000

    Task 5

---
+++ python-functions/5-prime.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
