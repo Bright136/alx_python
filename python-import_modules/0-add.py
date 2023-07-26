5cdf90ec23b82a313efdb69d314fc690ab1b8e78
Bright Eshun <eshunbright136@gmail.com>
2023-07-26 20:11:09 +0000

    made add_0 file unexecutable if imported

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,8 +2,7 @@
 
 from add_0 import add
 
-
 a = 1 
 b = 2 
-add_0 = add(a=1, b=2)
-print("{} + {} = {}".format(a, b, add_0))
+if __name__=="__main__":
+    print("{} + {} = {}".format(a, b, add(a=1, b=2)))
