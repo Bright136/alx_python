eee60e728dcab37b4c905bc3ee10e712527ac644
Bright Eshun <eshunbright136@gmail.com>
2023-07-27 20:11:24 +0000

    Task 0 for python data structures

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    list_string = list(my_string.lower())
+    for index, char in enumerate(list_string):
+        if char == 'c':
+            list_string[index] = ''
+    return ''.join(list_string)
+
+
+# print(no_c("Holberton School"))
+# print(no_c("Chicago"))
+# print(no_c("C is fun!"))
+            
