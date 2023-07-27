d1c75856aca46ac4bf3a5acf8a251fe7c245a7bf
Bright Eshun <eshunbright136@gmail.com>
2023-07-27 20:24:12 +0000

    Task 2 for  python data structures

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length_of_chars = len(sentence)
+    first_char = sentence[0]
+    return length_of_chars, first_char
+
+sentence = "At Holberton school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
+
