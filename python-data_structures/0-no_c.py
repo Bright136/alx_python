17f24061fd7412f87d60e7c17caa4aacdc36d6ee
Bright Eshun <eshunbright136@gmail.com>
2023-07-27 20:15:53 +0000

    updated Task 0

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    list_string = list(my_string.lower())
+    list_string = list(my_string)
     for index, char in enumerate(list_string):
-        if char == 'c':
+        if char == 'c' or char=='C':
             list_string[index] = ''
     return ''.join(list_string)
 
 
-# print(no_c("Holberton School"))
-# print(no_c("Chicago"))
-# print(no_c("C is fun!"))
+print(no_c("Holberton School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
             
