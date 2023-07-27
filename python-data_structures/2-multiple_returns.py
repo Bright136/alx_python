8d543ca75d040608292d7872550851bb23242f69
Bright Eshun <eshunbright136@gmail.com>
2023-07-27 20:25:45 +0000

    commented out examples

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -5,7 +5,7 @@ def multiple_returns(sentence):
     first_char = sentence[0]
     return length_of_chars, first_char
 
-sentence = "At Holberton school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+# sentence = "At Holberton school, I learnt C!"
+# length, first = multiple_returns(sentence)
+# print("Length: {:d} - First character: {}".format(length, first))
 
