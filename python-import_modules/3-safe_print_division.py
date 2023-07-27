12c1359504c71c53c7a481740dfed6fadb848b82
Bright Eshun <eshunbright136@gmail.com>
2023-07-27 10:22:03 -0700

    Removed print statement from Task 3 of import and modules

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -11,15 +11,3 @@ def safe_print_division(a, b):
         print(f"Inside result: {result}")
         return result
 
-a = 12
-b = 2
-
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
