7d4bc2366f72958888055cc3b32908f9fc0197a1
Bright Eshun <eshunbright136@gmail.com>
2023-07-27 20:36:09 +0000

    fixed first character for empty sentence - 2

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -4,11 +4,11 @@ def multiple_returns(sentence):
     first_char = sentence[0] if sentence else None
     return len(sentence), first_char
 
-sentence = "At Holberton school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+# sentence = "At Holberton school, I learnt C!"
+# length, first = multiple_returns(sentence)
+# print("Length: {:d} - First character: {}".format(length, first))
 
-sentence = ""
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+# sentence = ""
+# length, first = multiple_returns(sentence)
+# print("Length: {:d} - First character: {}".format(length, first))
 
