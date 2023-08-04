a8d37546f629bc81e93ab7cb4c385db31313e575
Bright Eshun <eshunbright136@gmail.com>
2023-08-04 16:45:13 +0000

    Task  1 for more data structures

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,4 @@
-# Write a function that computes the square value of all integers of a matrix.
-
-# Prototype: def square_matrix_simple(matrix=[]):
-# matrix is a 2 dimensional array
-# Returns a new matrix:
-# Same size as matrix
-# Each value should be the square of the value of the input
-# Initial matrix should not be modified
-# You are not allowed to import any module
-# You are allowed to use regular loops, map, etc.
+#!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
@@ -17,21 +8,7 @@ def square_matrix_simple(matrix=[]):
         new_matrix.append(new_list)
     return new_matrix
 
-    #     for i in range(len(arr)):
-    #         new_values = arr[i] ** 2
-    #     new_list.append(new_values)
-    #     new_matrix.append(new_list)
-    # return new_matrix
+
         
 
 
-# matrix = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
-# ]
-
-# new_matrix = square_matrix_simple(matrix)
-# print('new', new_matrix)
-# print('old', matrix)
-# # # [[1, 4, 9], [16, 25, 36], [49, 64, 81]]
