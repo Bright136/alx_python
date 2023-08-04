805561162e383d7318c35b6bafe3fe9740d1b812
Bright Eshun <eshunbright136@gmail.com>
2023-08-04 17:15:55 +0000

    Fixed task 3 for more data structures

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -2,7 +2,7 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or a_dictionary == {}:
         return None
     best_score_index = list(a_dictionary.values()).index(max(a_dictionary.values()))
     return list(a_dictionary.keys())[best_score_index]
