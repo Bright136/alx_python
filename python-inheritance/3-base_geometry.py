953173c1cfaa95802b3d1daa0ab6550934b83b9b
Bright Eshun <eshunbright136@gmail.com>
2023-08-11 18:19:11 +0000

    Task 3 of python inheritance

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+3-base_geometry - A module for BaseGeometry .
+
+This module provides classes for instantiating BaseGeometry.
+
+"""
+class BaseGeometry():
+    """
+        BaseGeometry - A class representing a basegeometry.
+
+        This is an empty class with no attributes
+    """
+
+    def __init__(self):
+        pass
+
+
