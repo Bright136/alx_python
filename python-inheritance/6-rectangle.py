2345365f08941f3e01048f5147c6dcb070d9f68b
Bright Eshun <eshunbright136@gmail.com>
2023-08-17 20:22:47 +0000

    modified code

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -36,20 +36,20 @@ class Rectangle(module):
         self.__height = height
         self.integer_validator('height', self.__height)
         self.integer_validator('width', self.__width)
-r = Rectangle(3, 5)
+# r = Rectangle(3, 5)
 
-print(r)
-print(dir(r))
+# print(r)
+# print(dir(r))
 
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+#     print("Rectangle: {} - {}".format(r.width, r.height))
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+#     r2 = Rectangle(4, True)
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
 
 
 
