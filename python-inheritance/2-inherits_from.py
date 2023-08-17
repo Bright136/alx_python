4161c52cbb9c5a37f80feaf4f6a8c980e5f12822
Bright Eshun <eshunbright136@gmail.com>
2023-08-17 19:24:14 +0000

    Modified task 2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -39,5 +39,14 @@ def inherits_from(obj, a_class):
         is_dog_animal = is_subclass(my_dog, Animal)  # Returns True
         is_cat_animal = is_subclass(my_cat, Animal)   # Returns True
     """
-    return issubclass(type(obj), a_class)
+
+    return issubclass(type(obj), a_class) and type(obj) != a_class
+
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
 
