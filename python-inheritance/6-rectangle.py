1d39141d6c6f182cfab7d7548fbbbfbcad79ca18
Bright Eshun <eshunbright136@gmail.com>
2023-08-17 19:39:56 +0000

    Redid task to drop __init_subclass__

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -12,6 +12,7 @@ Example:
 module_name = "5-base_geometry"  # Numeric module name
 module = __import__(module_name).BaseGeometry
 
+
 class Rectangle(module):
     """
     Rectangle class that inherits from BaseGeometry.
@@ -36,6 +37,25 @@ class Rectangle(module):
         self.__height = height
         self.integer_validator('height', self.__height)
         self.integer_validator('width', self.__width)
+
+
+
+
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
 
     
