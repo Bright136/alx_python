09c292c7624267b7fa8861c1ad0a18452d0613b6
Bright Eshun <eshunbright136@gmail.com>
2023-08-17 19:33:08 +0000

    redocumented codes

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-class CustomDirMeta(type):
-    def __dir__(cls):
-        # Customize the list of attributes and methods shown for the class
-        dir_list = super().__dir__()
-        dir_list.remove('__init_subclass__')
-        return dir_list
+
 """
 
 3-base_geometry - A module for BaseGeometry .
@@ -12,25 +7,55 @@ class CustomDirMeta(type):
 This module provides classes for instantiating BaseGeometry.
 
 """
+#!/usr/bin/python3
+
+class CustomDirMeta(type):
+    """
+    CustomDirMeta - A metaclass for customizing the dir() behavior.
+
+    This metaclass is used to customize the list of attributes and methods shown
+    for classes that inherit from it.
+
+    """
+
+    def __dir__(cls):
+        """
+        __dir__ - Customize the list of attributes and methods shown for the class.
+
+        This method is called when the dir() function is used on a class. It modifies
+        the list of attributes and methods returned by dir() to exclude the
+        '__init_subclass__' attribute.
+
+        :return: A filtered list of attributes and methods for the class.
+        :rtype: list
+        """
+        dir_list = super().__dir__()
+        dir_list.remove('__init_subclass__')
+        return dir_list
+
+"""
+3-base_geometry - A module for BaseGeometry.
+
+This module provides classes for instantiating BaseGeometry.
+"""
+
 class BaseGeometry(metaclass=CustomDirMeta):
     """
-        BaseGeometry - A class representing a basegeometry.
+    BaseGeometry - A class representing a base geometry.
 
-        This is an empty class with no attributes
+    This is an empty class with no attributes.
 
-    """   
+    """
 
-   
     def __dir__(self):
-        """ Override the default behavior of the dir() function for the class.
-            This method is called when the dir() function is used on an instance of the class.
-            It filters out the '__init_subclass__' attribute from the list of attributes and methods
-            returned by dir(), providing a customized view of the instance's attributes.
+        """
+        __dir__ - Override the default behavior of the dir() function for the class.
+
+        This method is called when the dir() function is used on an instance of the class.
+        It filters out the '__init_subclass__' attribute from the list of attributes and methods
+        returned by dir(), providing a customized view of the instance's attributes.
+
+        :return: A filtered list of attributes and methods for the instance.
+        :rtype: list
         """
         return [attr for attr in dir(type(self)) if attr != '__init_subclass__']
-    
-    
-# bg = BaseGeometry()
-# print(bg)
-# print(dir(bg))
-# print(dir(BaseGeometry))
