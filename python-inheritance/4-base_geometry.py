e08cbb726900a8a6c9a4c2aaabb61e1bbd2a9583
Bright Eshun <eshunbright136@gmail.com>
2023-08-17 11:30:43 +0000

    Added task 4

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,10 @@
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+# if __name__ == "__main__":
+#     bg = BaseGeometry()
+#     try:
+#         print(bg.area())
+#     except Exception as e:
+#         print("[{}] {}".format(e.__class__.__name__, e))
